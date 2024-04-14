0b6102ed63f8c7f11a001d22664ccf74ae17246b
joe20021313 <joseph20021970@gmail.com>
2024-04-14 04:17:59 +0100

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './Components/NavBar.jsx';
 function App() {
   return (
     <NavBar/>
+    
   );
 }
 
--- src/Pages/Joe.jsx
+++ src/Pages/Joe.jsx
@@ -1,13 +1,21 @@
 import "../App.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import mainBike from '../assets/proxy-image-Photoroom.jpeg';
 
 function Joe() {
   return (
-    <div style={{ paddingTop:"2rem"}}>
-
-<h1 style={{ color:"white", textAlign:"center"}}>dsds</h1>
+    <div style={{ 
+      paddingTop: "2rem", 
+      backgroundImage: `url(${mainBike})`, // Use backticks and ${} for string interpolation
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed' ,
+      height: '100vh',
+    }}>
+     
+      <h1 style={{ color: "white", textAlign: "center" }}>dsds</h1>
     </div>
-
   );
 }
 
